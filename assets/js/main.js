834c922aff4d4f29ec16d61e71c1a834bc439073
JWizerd <jwodke@outlook.com>
2017-08-19 12:42:06 -0600

    slideshow interval pause play functionality added with audio play pause functionality. ready for image automation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,71 @@
-function backgroundSlideShow(wallpaperCount, slideTransitions) {
-  var count = 1
-  setInterval(function(){
-    if (count > (wallpaperCount - 1)) {
-      count = 1
+// var start;
+// function backgroundSlideShow(wallpaperCount, slideTransitions) {
+//   var count = 1,
+//       timer = null,
+
+//   $('').on('click', function(){
+//     setInterval(function(wallpaperCount, slideTransitions){
+//       if (count > (wallpaperCount - 1)) {
+//         count = 1
+//       } else {
+//         $('.slide').fadeOut('slow', function() {
+//           $('.slide').attr('src', 'assets/img/' + count + '.jpg')
+//         })
+//         $('.slide').fadeIn('slow')
+//         count++
+//         console.log(count)
+//       }
+//     }, slideTransitions) 
+//   });
+// }
+
+// https://stackoverflow.com/questions/41756542/how-to-toggle-setinterval-on-click-javascript
+
+var timer    = null, 
+    interval = 1000,
+    value    = 0,
+    count    = 1
+    audio    = document.getElementById("audio")
+
+function backgroundSlideShow(slideCount, slideTransitions) {
+
+  console.log(count)
+  $("#start").click(function() {
+    console.log('firing')
+    audio.play()
+    // if timer is set break
+    if (timer !== null) {
+      return
     } else {
-      $('.slide').fadeOut('slow', function() {
-        $('.slide').attr('src', 'assets/img/' + count + '.jpg')
-      })
-      $('.slide').fadeIn('slow')
-      count++
-      console.log(count)
+      timer = setInterval(function() {
+        slideInterval(slideCount)
+      }, slideTransitions)  
     }
-  }, slideTransitions) 
+  });
+
+  $("#stop").click(function() {
+    audio.pause()
+    clearInterval(timer);
+    timer = null
+  });
+}
+
+function slideInterval(slideCount) {
+  console.log(count)
+  if (count == slideCount) {
+    count = 1
+    $('h1').html('"Gone Fishin"')
+    clearInterval(timer);
+    timer = null
+  } else {
+    $('.slide').fadeOut('slow', function() {
+      $('.slide').attr('src', 'assets/img/' + count + '.jpg')
+    })
+    $('.slide').fadeIn('slow')
+    count++
+  }
 }
 
 $(document).ready(function(){
   backgroundSlideShow(5, 8000)
 })
-
-// https://stackoverflow.com/questions/41756542/how-to-toggle-setinterval-on-click-javascript
